3cb621c93d05b8140deb2b9f9da8fc719ad729f6
Toby Miller <tobius.miller@gmail.com>
2013-10-14 15:37:56 -0400

    made child process into spawned pid and added data and close events

--- routes/render.js
+++ routes/render.js
@@ -1,25 +1,48 @@
-var childProcess = require('child_process');
-var path = require("path");
-var _ = require("underscore");
-var util = require("util");
 
-/*
- * GET PhantomJS Rendering
- */
-exports.index = function(req, res) {
-	var childArgs = [path.join(__dirname, '../rasterize.js'), "http://raphaeljs.com/polar-clock.html", "png"];
-	var execFile = process.env.PHANTOMJS_PATH || "node_modules/phantomjs/bin/phantomjs";
-	var delegate = res;
-	var child = childProcess.execFile(execFile, childArgs, function(err, stdout, stderr) {
-		var message = JSON.parse(stdout);
-		if (message.image) {
-			if (message.image) {
-				delegate.render('render', { image: message.image });
-				child.kill();
-			}
-		} else {
-			console.log("[stdout]",message);
-		}
-	});
+// node modules
+var _     = require('underscore'),
+    path  = require('path'),
+    spawn = require('child_process').spawn,
+    util  = require('util');
+
+// render index page
+exports.index = function(req, res){
+
+    var uri = req.query.uri || 'http://raphaeljs.com/polar-clock.html';
+    var args = [ path.join(__dirname, '../rasterize.js'), uri, 'png' ];
+    var exec = process.env.PHANTOMJS_PATH || 'node_modules/phantomjs/bin/phantomjs';
+    var phantomjs = spawn(exec, args);
+    var json = { image : '', error : '' };
+    var result = '';
+
+    // capture result
+    phantomjs.stdout.on('data', function(data){
+        result += data;
+    });
+
+    // capture errors
+    phantomjs.stderr.on('data', function(data){
+        json.error += data;
+    });
+
+    // close pid
+    phantomjs.on('close', function(code){
+
+        // test result
+        if (result !== ''){
+            result = JSON.parse(result);
+            if (result.image !== undefined){
+                json.image = result.image;
+            }
+        }
+
+        // render data
+        res.render('render', json);
+
+        // exit pid
+        phantomjs.kill( code === 0 ? 'SIGHUP' : 'SIGKILL');
+
+    });
 
 };
+
